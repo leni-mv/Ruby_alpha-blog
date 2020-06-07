bc6bbfcc29d81f73516076361302a053c2074cd5
mv <mvatinel38@gmail.com>
2020-06-07 21:08:52 +0200

    preparation à la mise en production done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'page/home', to: 'page#home'
+  root 'page#home'
 
   get 'page/about', to: 'page#about'
 
