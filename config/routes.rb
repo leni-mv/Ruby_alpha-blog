43d71cd738f00d8382e06e50a46d4031fc09f6d4
mv <mvatinel38@gmail.com>
2020-06-07 18:35:40 +0200

    add mvc page<home/about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'page/home', to: 'page#home'
+
+  get 'page/about', to: 'page#about'
+
 end
